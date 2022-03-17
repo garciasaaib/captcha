aa2df83271354782c5bf932ccb21411c32684e11
Adrian Garcia <garciasaaib@gmail.com>
2022-03-16 20:15:37 -0600

    add and remove from select is workin now

--- src/componets/CaptchaImg/index.jsx
+++ src/componets/CaptchaImg/index.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 import { ContainerCube } from '../ContainerCube'
 import { CubeImg } from '../CubeImg'
 import { DirectionsCaptchaImg } from '../DirectionsCaptchaImg'
+import imagesObject from "../../helpers/data";
 const StyledCaptchaImg = styled.div`
   width: 100%;
   max-width: 500px;
@@ -10,51 +11,57 @@ const StyledCaptchaImg = styled.div`
   border: 1px solid white;
 `;
 export const CaptchaImg = () => {
-  // const data = getRandomFacts()
-  let data
-  data = {
-    category: "cats",
-    images: [
-      {category:"cats", url:"https://i.ibb.co/j5D8xwq/cat1.jpg"},
-      {category:"cats", url:"https://i.ibb.co/s9CcNg1/cat2.jpg"},
-      {category:"cats", url:"https://i.ibb.co/mSYBwgC/cat3.jpg"},
-      {category:"cats", url:"https://i.ibb.co/XtGxmJ7/cat4.jpg"},
-      {category:"mouse", url:"https://i.ibb.co/TwxBcZ8/cat5.jpg"},
-      {category:"dogs", url:"https://i.ibb.co/b2RNzdh/cat6.jpg"},
-      {category:"dogs", url:"https://i.ibb.co/ZL4ZSCD/cat7.jpg"},
-      {category:"dogs", url:"https://i.ibb.co/T1knJvT/cat8.jpg"},
-      {category:"dogs", url:"https://i.ibb.co/DgLwd9x/cat9.jpg"},
-  ]}
-  const {images, category} =data
+  // Init fetched data and selected images
+  const [fetchedData, setFetchedData] = useState(new Array(0))
+  const {images, category} = fetchedData
   const [selected, setSelected] = useState(new Array(0))
+  useEffect(() => {
+    fetchData()
+  },[])
+
+
+  // Get the data from the captcha
+  async function fetchData() {
+    const newData = await imagesObject()
+    setFetchedData(newData)
+  }
+
+  // Check if it is a valid combination
   const handlerSubmit = (e) => {
     e.preventDefault()
     // console.log(selected.length)
     console.log(selected)
-    if(selected.length < 4) return alert('Select 4 images at least')
+    // if(selected.length < 4) return alert('Select 4 images at least')
   }
-  const toggleSelect = (boxSelected) => {
-    const alreadyExist = data.images.filter((row) => {
-      return row.url
-    })
-    console.log(alreadyExist)
-    // agrega todo seleccionado
-    setSelected([...selected,boxSelected])
 
-    // !alreadyExist 
+  // Add and remove positions in array
+  const toggleSelect = (boxSelected) => {
+    const alreadyExist = selected.find((row) => {
+      return row.url === boxSelected.url
+    })
+    if(!alreadyExist) setSelected([...selected,boxSelected])
+    else {
+      const index = selected.indexOf(alreadyExist)
+      const newSelectedState = [...selected]
+      newSelectedState.splice(index, 1)
+      setSelected(newSelectedState)
+    }
+    // agrega todo seleccionado
+
+    // !alreadyExist
     //   ? setSelected([...selected,boxSelected])
     //   : console.log("ya existe")
     // console.log(boxSelected)
     // const newSelected = selectedboxSelected)
     // console.log()
-    
   }
+
   return (
     <StyledCaptchaImg>
       <h1>Select all the {category} pictures</h1>
       <form onSubmit={handlerSubmit}>
-        <ContainerCube>
-        {images.map((image, id) =>
+        <ContainerCube images={images}>
+        {images?.map((image, id) =>
           <CubeImg
             key={id}
             {...image}
@@ -65,7 +72,7 @@ export const CaptchaImg = () => {
         <input type="submit" value="Send Response" />
       </form>
 
-      <DirectionsCaptchaImg />
+      <DirectionsCaptchaImg refresh={fetchData}/>
     </StyledCaptchaImg>
   )
 }//rafc
---
+++ src/helpers/data.js
@@ -0,0 +1,51 @@
+// Import Database
+const database = require("./imagenes.json")
+
+function generateImagesObject() {
+  // Define random category
+  const randomCategory = randomfrom(0, 4)
+  const category = database.categories[randomCategory]
+  var imagesArray = [];
+  var imagesObject = {};
+
+
+  // Define Random images from category defined
+  var index = randomfrom(0, 8);
+  imagesArray[index] = database.images[randomCategory][randomfrom(0, 19)];
+  for (let i = 1; i <= 4; i++) {
+    while (imagesArray[index]) {
+      index = randomfrom(0, 8);
+    }
+    imagesArray[index] = database.images[randomCategory][randomfrom(0, 19)];
+  }
+
+
+  // Define Random images from other categories
+  var randomFalseCat = randomCategory;
+  for (let i = 0; i <= 8; i++) {
+    while (randomFalseCat === randomCategory) {
+      randomFalseCat = randomfrom(0, 4)
+      // console.log("2nd While")
+    }
+
+    if (!imagesArray[i]) {
+      imagesArray[i] = database.images[randomFalseCat][randomfrom(0, 19)];
+    }
+  }
+
+  // Build JSON to export
+
+  imagesObject.category = category;
+  imagesObject.images = imagesArray;
+
+  // Export JSON object {"Categoy":"xxx","images":["","","","","","","","",""]}
+
+  // export default imagesObject;
+  return imagesObject;
+}
+export default generateImagesObject;
+// Support Function
+function randomfrom(min, max) {
+  var randomNumber = Math.floor(Math.random() * max + min)
+  return randomNumber;
+}
